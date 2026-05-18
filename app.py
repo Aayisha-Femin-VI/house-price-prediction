6b9fdbd9256d5a39d12bd54a678ec43571547f7e
Aayisha-Femin-VI <u2403010@rajagiri.edu.in>
2026-05-18 13:33:57 +0530

    first comment

--- app.py
+++ app.py
@@ -2,52 +2,44 @@ from flask import Flask, render_template, request
 import pandas as pd
 import joblib
 
-# Create Flask app
 app = Flask(__name__)
 
-# Load trained model
+# Load model
 model = joblib.load("gradient_boost_model.pkl")
 
-# Load training columns
+# Load columns
 model_columns = joblib.load("model_columns.pkl")
 
 
-# Home page
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
-# Prediction page
 @app.route("/predict", methods=["POST"])
 def predict():
 
     try:
-        # Get user inputs from HTML form
-        input_dict = {
-            "OverallQual": float(request.form["OverallQual"]),
-            "GrLivArea": float(request.form["GrLivArea"]),
-            "GarageCars": float(request.form["GarageCars"]),
-            "TotalBsmtSF": float(request.form["TotalBsmtSF"]),
-            "FullBath": float(request.form["FullBath"]),
-            "YearBuilt": float(request.form["YearBuilt"])
-        }
 
-        # Convert input into dataframe
-        input_data = pd.DataFrame([input_dict])
+        # Get inputs
+        overall_qual = float(request.form["OverallQual"])
+        gr_liv_area = float(request.form["GrLivArea"])
+        garage_cars = float(request.form["GarageCars"])
+        total_bsmt_sf = float(request.form["TotalBsmtSF"])
+        full_bath = float(request.form["FullBath"])
+        year_built = float(request.form["YearBuilt"])
 
-        # Convert categorical columns if needed
-        input_data = pd.get_dummies(input_data)
+        # Create dataframe
+        input_data = pd.DataFrame([[
+            overall_qual,
+            gr_liv_area,
+            garage_cars,
+            total_bsmt_sf,
+            full_bath,
+            year_built
+        ]], columns=model_columns)
 
-        # Add missing columns
-        for col in model_columns:
-            if col not in input_data.columns:
-                input_data[col] = 0
-
-        # Keep same column order
-        input_data = input_data[model_columns]
-
-        # Prediction
+        # Predict
         prediction = model.predict(input_data)
 
         output = round(prediction[0], 2)
@@ -65,6 +57,5 @@ def predict():
         )
 
 
-# Run Flask app
 if __name__ == "__main__":
     app.run(debug=True)
